058f5f71ae9e23e3a25c174a5c96fa8ea6794c8f
Jabin <2099008974@qq.com>
2020-08-03 21:34:52 +0800

    [reacter-counter][jabin] feature: add onDecrease and onIncrease of Counter component

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
- 
-class Counter extends React.Component{
-    constructor(props){
+
+class Counter extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            value : 0
+            value: 0
         }
     }
-
-    render(){
-        return(
-        <div>
-            <button>-</button>
-            <mark>{this.state.value}</mark>
-            <button>+</button>
-        </div>);
+    onDecrease = () => {
+        this.setState((prevState) => ({
+            value: prevState.value - 1
+        }));
+    }
+    onIncrease = () => {
+        this.setState((prevState) =>
+            ({
+                value: prevState.value + 1
+            })); 
+            
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.onDecrease}>-</button>
+                <mark>{this.state.value}</mark>
+                <button onClick={this.onIncrease}>+</button>
+            </div>);
     }
 
 }
